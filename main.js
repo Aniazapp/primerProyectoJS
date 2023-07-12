dc3e356cd1adf67f7d9aea405a6b156068ce2ca6
Aniazapp <aniayazmin1@gmail.com>
2023-07-12 15:18:41 -0300

    proyecto terminado

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 let share = document.getElementById("share");
+let darkShare = document.getElementById("darkShare");
 
 
 
 function manejarBoton(){
     let vineta = document.getElementById("vineta");
     let estilo= window.getComputedStyle(vineta)
-    
     if (estilo.display=="none"){
         vineta.style.display = "flex"
+        share.style.display = "none"
+        darkShare.style.display = "flex"
     } else{
         vineta.style.display = "none"
+        share.style.display = "flex"
+        darkShare.style.display = "none"
     }
 
 }
